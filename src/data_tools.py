0da7a6ef79dd8ba1ebe72fcd46c16e38a038b332
jordan403 <jor.smith@reply.com>
2022-11-28 11:13:24 +0000

    import historic CI data for grid

--- src/data_tools.py
+++ src/data_tools.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import os
 
 
 
@@ -20,3 +22,27 @@ def generate_dict_y_coords_for_kW(y_grid_line_coords, y_values_on_graph=[4,2,0,-
 
     y_coord_dict = {y_val: calculate_pixel_location(y_val, kw_per_pixel, baseline_y) for y_val in y_values_on_graph}
     return y_coord_dict, baseline_y, kw_per_pixel
+
+
+
+def generate_csv_all_data():
+    
+    """ 
+    generate a csv with concatenation of all csv data in data/grid/ directory
+    
+    """
+    
+    relative_path_to_grid_data = os.path.join("data","grid_data")
+    all_csv = [name for name in os.listdir(relative_path_to_grid_data) if name.endswith(".csv")]
+    cwd = os.getcwd()
+
+    df_list = []
+
+    for csv_name in all_csv:
+
+        path = os.path.join(cwd, relative_path_to_grid_data, csv_name)
+        df_list.append(pd.read_csv(path))
+
+    all_data = pd.concat(df_list, ignore_index=True).drop_duplicates()
+    
+    return all_data
